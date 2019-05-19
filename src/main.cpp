3b7e8e7bff6aa18262dc9c72b81a7d8ad314f121
Kenny McCormick <keennymccormic+github@gmail.com>
2019-05-18 19:21:51 -0500

    remove old code from main

--- src/main.cpp
+++ src/main.cpp
@@ -8,67 +8,18 @@
 #include "SFLCARS.hpp"
 #include "interface/TextBar.hpp"
 
-/*
-const float edgePadding = 20.0f;
-const float padding = 10.0f;
-
-int windowWidth = 600;
-int windowHeight = 400;
-
-float width = 0.0f;
-float height = 0.0f;
-
-const float barEdgeWidth = 50;
-const float barHeight = 35;
-
-sf::RoundedRectangleShape s1;
-sf::RectangleShape ss1;
-
-sf::RectangleShape s2;
-
-sf::RoundedRectangleShape s3;
-sf::RectangleShape ss3;
-
-sf::Text t;
-
-void updatePositions()
-{
-	width = windowWidth - (edgePadding * 2);
-	height = windowHeight - (edgePadding * 2);
-
-	s1.setSize(sf::Vector2f(barEdgeWidth, barHeight));
-	s1.setCornerPointCount(16);
-	s1.setCornersRadius(16);
-
-	ss1.setSize(sf::Vector2f(barEdgeWidth / 2, barHeight));
-
-	s2.setSize(sf::Vector2f(width - ((barEdgeWidth * 2) + (padding * 3) + t.getGlobalBounds().width), barHeight));
-
-	s3.setSize(sf::Vector2f(barEdgeWidth, barHeight));
-	s3.setCornerPointCount(16);
-	s3.setCornersRadius(16);
-
-	ss3.setSize(sf::Vector2f(barEdgeWidth / 2, barHeight));
-
-	s1.setPosition(edgePadding, edgePadding);
-	ss1.setPosition(edgePadding + (barEdgeWidth / 2), edgePadding);
-	t.setPosition(s1.getPosition().x + s1.getSize().x + padding, edgePadding - 7);
-	s2.setPosition(t.getPosition().x + t.getGlobalBounds().width + padding, edgePadding);
-	s3.setPosition(s2.getPosition().x + s2.getSize().x + padding, edgePadding);
-	ss3.setPosition(s3.getPosition().x, edgePadding);
-}
-*/
-
 int main()
 {
 	SFLCARS* lcars = new SFLCARS;
 	Display* display = lcars->newDisplay(sf::VideoMode(600, 400), sf::Vector2i(100, 100));
 
 	TextBar bar("SFLCARS");
-	Bar bar2;
+	TextBar bar2("bar2");
+	TextBar bar3("bar3");
 
 	display->addElement(&bar);
 	display->addElement(&bar2);
+	display->addElement(&bar3);
 
 	while (lcars->isRunning())
 	{
