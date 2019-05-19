c266529914f1bc79dd3ed49184e187a675e09484
Kenny McCormick <keennymccormic+github@gmail.com>
2019-05-18 19:24:46 -0500

    remove more old code from main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
-#include <iostream>
+#include "SFLCARS.hpp"
+#include "interface/TextBar.hpp"
 
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 
-#include "utility/RoundRectangle.hpp"
-
-#include "SFLCARS.hpp"
-#include "interface/TextBar.hpp"
+#include <iostream>
 
 int main()
 {
@@ -29,79 +27,4 @@ int main()
 	}
 
 	delete lcars;
-
-	/*
-	sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "SFLCARS");
-
-	sf::Font font;
-	font.loadFromFile("./interface/resources/fonts/Okuda.otf");
-
-	t.setFont(font);
-	t.setString("SFLCARS");
-	t.setCharacterSize(36);
-
-	sf::SoundBuffer buffer;
-	if (!buffer.loadFromFile("./interface/resources/sounds/beep.ogg"))
-		std::cerr << "failed to load audio file" << std::endl;
-
-	sf::SoundBuffer buffer2;
-	if (!buffer2.loadFromFile("./interface/resources/sounds/beep2.ogg"))
-		std::cerr << "failed to load audio file" << std::endl;
-
-	std::vector<sf::Sound> sounds;
-
-	sf::Sound beep;
-	beep.setBuffer(buffer);
-	beep.setVolume(50);
-
-	updatePositions();
-
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::EventType::Closed)
-			{
-				window.close();
-			}
-			else if (event.type == sf::Event::EventType::Resized)
-			{
-				// update the view to the new size of the window
-				sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
-				window.setView(sf::View(visibleArea));
-
-				windowWidth = event.size.width;
-				windowHeight = event.size.height;
-
-				updatePositions();
-			}
-			else if (event.type == sf::Event::EventType::MouseButtonPressed)
-			{
-				sf::Sound beep;
-				beep.setBuffer(buffer);
-				beep.setVolume(50);
-				sounds.push_back(beep);
-				sounds.back().play();
-			}
-			else if (event.type == sf::Event::EventType::KeyPressed)
-			{
-				sf::Sound beep;
-				beep.setBuffer(buffer2);
-				beep.setVolume(50);
-				sounds.push_back(beep);
-				sounds.back().play();
-			}
-		}
-
-		window.clear();
-		window.draw(s1);
-		window.draw(ss1);
-		window.draw(s2);
-		window.draw(s3);
-		window.draw(ss3);
-		window.draw(t);
-		window.display();
-	}
-	*/
 }
