16dbd2ed0233bd22499da621971ac37cefdcbd9a
Kenny McCormick <keennymccormic+github@gmail.com>
2019-09-16 20:36:08 -0500

    bring in more Layout code

--- SFLCARS-interface/Layout.hpp
+++ SFLCARS-interface/Layout.hpp
@@ -21,6 +21,23 @@ public:
 
 	Element* addElement(Element* element);
 
+	Element* triggered = nullptr;
+	Element* focused = nullptr;
+	Element* hovered = nullptr;
+
+protected:
+	void onStateChanged(State state);
+
+	bool focusElement(Element* element, State state);
+
+	void onMouseMoved(const sf::Vector2f& position);
+	void onMousePressed(const sf::Vector2f& position);
+	void onMouseReleased(const sf::Vector2f& position);
+	void onMouseWheelMoved(int delta);
+	void onKeyPressed(const sf::Keyboard::Key& key);
+	void onKeyReleased(const sf::Keyboard::Key& key);
+	void onTextEntered(const sf::Uint32& unicode);
+
 private:
 	Display* parentDisplay;
 };
