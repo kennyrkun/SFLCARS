d6abefa329930f032a2035bf653f5d92e85bc9e8
Kenny McCormick <keennymccormic+github@gmail.com>
2019-09-21 17:13:00 -0500

    add SFLCARS-utility project
    has things that might be needed across projects. I don't want two version of the same code

---
+++ SFLCARS-utility/Password.cpp
@@ -0,0 +1,55 @@
+#include "PicoSHA256.hpp"
+#include "SettingsParser.hpp"
+
+#include <string>
+#include <ctime>
+
+namespace sflcars
+{
+namespace utility
+{
+
+namespace password
+{
+	std::string generateHashedPassword(const std::string& username, const std::string& password)
+	{
+		std::time_t now = time(0);
+
+		std::stringstream ss;
+		ss << now;
+
+		std::string hash_hex_str = picosha2::hash256_hex_string(password + ss.str());
+
+		SettingsParser parser("./resources/admin/" + username + ".dat");
+		parser.set("hash", hash_hex_str);
+		parser.set("salt", ss.str());
+
+		return hash_hex_str;
+	}
+
+	std::string getSavedPasswordHash(const std::string& username)
+	{
+		SettingsParser parser("./resources/admin/" + username + ".dat");
+
+		std::string savedPasswordHash;
+		parser.get("hash", savedPasswordHash);
+
+		return savedPasswordHash;
+	}
+
+	bool validatePassword(const std::string& username, const std::string& password)
+	{
+		std::string salt = "unable to find salt";
+
+		SettingsParser parser("./resources/admin/" + username + ".dat");
+		parser.get("salt", salt);
+
+		std::string providedPasswordHash = picosha2::hash256_hex_string(password + salt);
+		std::string savedPasswordHash = getSavedPasswordHash(username);
+
+		return providedPasswordHash == savedPasswordHash;
+	}
+}
+
+}
+}
---
+++ SFLCARS-utility/Password.hpp
@@ -0,0 +1,23 @@
+#ifndef PASSWORD_HPP
+#define PASSWORD_HPP
+
+#include <string>
+
+namespace sflcars
+{
+namespace utility
+{
+
+namespace password
+{
+    std::string generateHashedPassword(const std::string& username, const std::string& password);
+
+    std::string getSavedPasswordHash(const std::string& username);
+
+    bool validatePassword(const std::string& username, const std::string& password);
+}
+
+}
+}
+
+#endif // !PASSWORD_HPP
