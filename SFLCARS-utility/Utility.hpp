f20d1649d49abf108492e66706d12ee4c86da944
Kenny McCormick <keennymccormic+github@gmail.com>
2019-09-21 19:59:18 -0500

    add getRandomNumber to SFLCARS-utility

---
+++ SFLCARS-utility/Utility.hpp
@@ -0,0 +1,14 @@
+#ifndef SFLCARS_UTILITY_HPP
+#define SFLCARS_UTILITY_HPP
+
+namespace sflcars
+{
+
+namespace utility
+{
+	size_t getRandomNumber(size_t min, size_t max);
+}
+
+}
+
+#endif // !SFLCARS_UTILITY_HPP
