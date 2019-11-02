b2ecbbd34b4c8efec269f92b2723cb5cab1c6b57
Kenny McCormick <keennymccormic+github@gmail.com>
2019-11-02 12:09:52 -0500

    add convertToTimestamp function to Utility

--- SFLCARS-utility/Utility.cpp
+++ SFLCARS-utility/Utility.cpp
@@ -1,6 +1,7 @@
 #include "Utility.hpp"
 
 #include <random>
+#include <string>
 
 namespace sflcars
 {
@@ -14,6 +15,34 @@ namespace utility
 		std::uniform_int_distribution<std::mt19937::result_type> dist(min, max);
 
 		return dist(rng);
+	}
+	std::string convertToTimestamp(const time_t& time)
+	{
+		time_t $time = time;
+
+		int seconds = $time % 60;
+		$time /= 60;
+
+		int minutes = $time % 60;
+		$time /= 60;
+
+		int hours = $time % 24;
+		$time /= 24;
+
+		std::string seconds_s(std::to_string(seconds));
+		std::string minutes_s(std::to_string(minutes));
+		std::string hours_s(std::to_string(hours));
+
+		if (seconds < 10)
+			seconds_s.insert(0, "0");
+
+		if (minutes < 10)
+			minutes_s.insert(0, "0");
+
+		if (hours < 10)
+			hours_s.insert(0, "0");
+
+		return (hours_s + ":" + minutes_s + ":" + seconds_s);
 	}
 }
 
--- SFLCARS-utility/Utility.hpp
+++ SFLCARS-utility/Utility.hpp
@@ -1,12 +1,17 @@
 #ifndef SFLCARS_UTILITY_HPP
 #define SFLCARS_UTILITY_HPP
 
+#include <string>
+#include <ctime>
+
 namespace sflcars
 {
 
 namespace utility
 {
 	size_t getRandomNumber(size_t min, size_t max);
+
+	std::string convertToTimestamp(const time_t& time);
 }
 
 }
