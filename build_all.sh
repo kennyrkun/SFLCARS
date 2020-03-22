44a937ba1b5083c888ea89cd14b00d925efe6081
kennyrkun <kennyrmccormick@gmail.com>
2020-03-22 15:34:05 -0500

    move libraries into main directory and use local libraries at runtime

--- build_all.sh
+++ build_all.sh
@@ -1,13 +1,13 @@
 cd SFLCARS-utility
 cmake ./
 make
-mv libsflcars-utility.so /usr/lib/libsflcars-utlity.so
+mv libsflcars-utility.so ../SFLCARS-main/libsflcars-utility.so
 
 cd ../
 cd SFLCARS-interface
 cmake ./
 make
-mv libsflcars-interface.so /usr/lib/libsflcars-interface.so
+mv libsflcars-interface.so ../SFLCARS-main/libsflcars-interface.so
 ln -s ../SFLCARS-interface/resources ../SFLCARS-main/resources
 
 cd ../
